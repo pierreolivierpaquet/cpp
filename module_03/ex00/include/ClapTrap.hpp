565ad2eeaaa7132234b96919a6a6528042b7a5c0
Pierre Olivier Paquet <ppaquet@c1r4p8.42quebec.com>
2024-01-17 15:47:13 -0500

    module_03/ex00 creation

---
+++ module_03/ex00/include/ClapTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 15:30:59 by ppaquet           #+#    #+#             */
+/*   Updated: 2024/01/17 15:46:30 by ppaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		CLAPTRAP_HPP_
+# define	CLAPTRAP_HPP_
+
+# include	<iostream>
+
+class ClapTrap {
+	private:
+	///
+	
+	public:
+		ClapTrap( void );
+		~ClapTrap( void );
+};
+
+#endif	/*	CLAPTRAP_HPP_	*/
