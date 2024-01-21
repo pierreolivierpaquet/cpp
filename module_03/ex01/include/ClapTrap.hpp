21f04fd5ca8c0daf878906b7e72ebd9928abcd66
Pierre Olivier <pierreolivierpaquet@hotmail.com>
2024-01-20 22:21:23 -0500

    Worked on module_03/ex01

--- module_03/ex01/include/ClapTrap.hpp
+++ module_03/ex01/include/ClapTrap.hpp
@@ -5,8 +5,127 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/19 12:20:33 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/01/19 12:20:34 by ppaquet          ###   ########.fr       */
+/*   Created: 2024/01/17 15:30:59 by ppaquet           #+#    #+#             */
+/*   Updated: 2024/01/20 21:04:14 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef		CLAPTRAP_HPP_
+# define	CLAPTRAP_HPP_
+
+# include	<iostream>
+
+/******************************************************************************/
+/******************************************************************************/
+
+# ifndef	EMPTY_STR
+#  define	EMPTY_STR	""
+# endif	/*	EMPTY_STR	*/
+
+# ifndef	EXIT_SUCCESS
+#  define	EXIT_SUCCESS 0
+# endif	/*	EXIT_SUCCESS	*/
+
+/********************************** MESSAGES **********************************/
+
+# ifndef	MSG_CLAPTRAP
+#  define	MSG_CLAPTRAP	"ClapTrap "
+# endif	/*	MSG_CLAPTRAP	*/
+
+# ifndef	MSG_ATTACKS
+#  define	MSG_ATTACKS		" attacks "
+# endif	/*	MSG_ATTACKS	*/
+
+# ifndef	MSG_CAUSING
+#  define	MSG_CAUSING		", causing "
+# endif	/*	MSG_CAUSING	*/
+
+# ifndef	MSG_DAMAGE
+#  define	MSG_DAMAGE		" points of damage!"
+# endif	/*	MSG_DAMAGE	*/
+
+# ifndef	MSG_CONSTRUCTOR
+#  define	MSG_CONSTRUCTOR " Constructor called."
+# endif	/*	MSG_CONSTRUCTOR	*/
+
+# ifndef	MSG_DESTRUCTOR
+#  define	MSG_DESTRUCTOR " Destructor called."
+# endif	/*	MSG_DESTRUCTOR	*/
+
+/*********************************** POINTS ***********************************/
+
+/* Amount of points recovered during life regeneration. */
+# ifndef	DEFAULT_HIT
+#  define	DEFAULT_HIT	10
+# endif	/*	DEFAULT_HIT	*/
+
+/* Energy level. */
+# ifndef	DEFAULT_ENERGY
+#  define	DEFAULT_ENERGY	10
+# endif	/*	DEFAULT_ENERGY	*/
+
+/* Number of damage point(s) suffered by the enemy. */
+# ifndef	DEFAULT_ATTACK
+#  define	DEFAULT_ATTACK	0
+# endif	/*	DEFAULT_ATTACK	*/
+
+# ifndef	COST_ATTACK
+#  define	COST_ATTACK	1
+# endif	/*	COST_ATTACK	*/
+
+# ifndef	COST_REPAIR
+#  define	COST_REPAIR	1
+# endif	/*	COST_REPAIR	*/
+
+/******************************************************************************/
+/******************************************************************************/
+
+class ClapTrap {
+	private:
+		std::string	_name;
+		/// @note Decreases when enemy attack, or increases from repairing.
+		int			_hit_point;
+		/// @note Decreases by <COST> when attacking, or repairing.
+		int			_energy_point;
+		/// @note Affects enemy's hit points when attacking.
+		int			_attack_damage;
+	
+	public:
+		ClapTrap	&operator=( const ClapTrap &rhs ); // Assignation overload.
+		ClapTrap	( const ClapTrap &source ); // Copy constructor.
+		ClapTrap	( void ); // Default constructor.
+		~ClapTrap	( void ); // Default destructor.
+
+		ClapTrap	( const std::string name ); // Parametric constructor.
+
+		/* ACTION FUNCTION */
+		void	attack		( const std::string &target );
+		void	takeDamage	( unsigned int amount );
+		void	beRepaired	( unsigned int amount );
+
+		/* SETTER+GETTER */
+		std::string	getName			( void ) const;
+		int			getHitPoint		( void ) const;
+		int			getEnergyPoint	( void ) const;
+		int			getAttackDamage	( void ) const;
+
+		void	setName( const std::string name );
+		void	setHitPoint( const int points );
+		void	setEnergyPoint( const int points );
+		void	setAttackDamage( const int points );
+		void	subEnergyPoint( const int points );
+		void	subHitPoint( const int points );
+		void	addHitPoints( const int points );
+		
+}	/*	ClapTrap	*/;
+
+#endif	/*	CLAPTRAP_HPP_	*/
+
+/*
+
+Quand ClapTrap attaque, sa cible perd <attack damage> hit points.
+Quand ClapTrap se répare, il regagne <amount> hit points.
+Les actions attaquer et réparer coûtent chacune 1 point d’énergie.
+Bien entendu, ClapTrap ne peut exécuter aucune action s’il n’a plus de vie ou d’énergie
+
+*/
--- module_03/ex01/src/ClapTrap.cpp
+++ module_03/ex01/src/ClapTrap.cpp
@@ -5,8 +5,169 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/19 12:20:35 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/01/19 12:20:36 by ppaquet          ###   ########.fr       */
+/*   Created: 2024/01/17 15:28:26 by ppaquet           #+#    #+#             */
+/*   Updated: 2024/01/20 22:00:51 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/ClapTrap.hpp"
+
+/******************************************************************************/
+/******************************* SETTER+GETTER ********************************/
+
+std::string	ClapTrap::getName( void ) const {
+	return ( this->_name );
+}
+
+int	ClapTrap::getHitPoint( void ) const {
+	return( this->_hit_point );
+}
+
+int	ClapTrap::getEnergyPoint( void ) const {
+	return ( this->_energy_point );
+}
+
+int	ClapTrap::getAttackDamage( void ) const {
+	return ( this->_attack_damage );
+}
+
+void	ClapTrap::setName( const std::string name ) {
+	this->_name = name;
+	return ;
+}
+
+void	ClapTrap::setHitPoint( const int points ) {
+	this->_hit_point = points;
+	return ;
+}
+
+void	ClapTrap::setEnergyPoint( const int points ) {
+	this->_energy_point = points;
+	return ;
+}
+
+void	ClapTrap::setAttackDamage( const int points ) {
+	this->_attack_damage = points;
+}
+
+/// @brief Substracts the <amount of points> to the instance's energy. 
+void	ClapTrap::subEnergyPoint( const int points ) {
+	this->_energy_point -= points;
+	return ;
+}
+
+/// @brief Substracts the <amount of points> to the instance's Hit Points.
+void	ClapTrap::subHitPoint( const int points ) {
+	this->_hit_point -= points;
+	return ;
+}
+
+/// @brief Adds the <amount of points> to the instance's Hit Points).
+void	ClapTrap::addHitPoints( const int points ) {
+	this->_hit_point += points;
+	return ;
+}
+
+/******************************************************************************/
+/*************************** PARAMETRIC CONSTRUCTOR ***************************/
+
+ClapTrap::ClapTrap( const std::string name ) :
+	_name			( name ),
+	_hit_point		( DEFAULT_HIT ),
+	_energy_point	( DEFAULT_ENERGY ),
+	_attack_damage	( DEFAULT_ATTACK ) {
+	std::cout	<< MSG_CLAPTRAP << this->getName()
+				<< MSG_CONSTRUCTOR << " [parametric]" << std::endl;
+	return ;
+}
+
+/******************************************************************************/
+/**************************** CANONICAL REQUISITE *****************************/
+
+/// @returns Reference to the updated current instance. 
+ClapTrap	&ClapTrap::operator=( const ClapTrap &rhs ) {
+	this->_name				=	rhs.getName();
+	this->_hit_point		=	rhs.getHitPoint();
+	this->_energy_point		=	rhs.getEnergyPoint();
+	this->_attack_damage	=	rhs.getAttackDamage();
+	return ( *this );
+}
+
+/// @brief Instance contructor based on another instance reference.
+ClapTrap::ClapTrap( const ClapTrap &source ) {
+	*this = source;
+	std::cout	<< MSG_CLAPTRAP << this->_name
+				<< MSG_CONSTRUCTOR << " [reference]" << std::endl;
+	return ;
+}
+
+/******************************************************************************/
+/********************** DEFAULT CONSTRUCTOR/DESTRUCTOR ************************/
+
+ClapTrap::ClapTrap( void ) :
+	_name			( EMPTY_STR ),
+	_hit_point		( DEFAULT_HIT ),
+	_energy_point	( DEFAULT_ENERGY ),
+	_attack_damage	( DEFAULT_ATTACK ) {
+	std::cout << "ClapTrap" << MSG_CONSTRUCTOR << " [default]" << std::endl;
+	return ;
+}
+
+ClapTrap::~ClapTrap( void ) {
+	std::cout	<< MSG_CLAPTRAP << this->getName()
+				<< MSG_DESTRUCTOR << std::endl;
+	return ;
+}
+
+/******************************************************************************/
+
+void	ClapTrap::attack( const std::string &target ) {
+	if (this->getHitPoint() <= 0){
+		std::cout	<< MSG_CLAPTRAP << this->getName()
+					<< " can't attack: It's dead." << std::endl;
+	} else if (this->getEnergyPoint() <= 0) {
+		std::cout	<< MSG_CLAPTRAP << this->getName()
+					<< " can't attack: No energy left." << std::endl;
+	} else {
+		std::cout	<< MSG_CLAPTRAP << this->getName()
+					<< MSG_ATTACKS << target << MSG_CAUSING
+					<< this->getAttackDamage() << MSG_DAMAGE << std::endl;
+		this->subEnergyPoint( COST_ATTACK );
+	}
+	return ;
+}
+
+void	ClapTrap::takeDamage( unsigned int amount ) {
+	if (amount == 0) {
+		return ;
+	} else {
+		this->subHitPoint( amount );
+		if (this->getHitPoint() < 0){
+			this->setHitPoint( 0 );
+		}
+		std::cout	<< MSG_CLAPTRAP << this->getName()
+					<< " took -" << amount << " of hit damage: It now has "
+					<< this->getHitPoint() << " hit points." << std::endl;
+	}
+	return ;
+}
+
+void	ClapTrap::beRepaired( unsigned int amount ) {
+	if (amount == 0){
+		return ;
+	} else if (this->getEnergyPoint() <= 0){
+		std::cout	<< MSG_CLAPTRAP << this->getName()
+					<< " can't be repaired: Not enough energy." << std::endl;
+	} else if (this->getHitPoint() <= 0) {
+		std::cout	<< MSG_CLAPTRAP << this->getName()
+					<< " can't be repaired: It's dead." << std::endl;
+	} else {
+		this->addHitPoints( amount );
+		this->subEnergyPoint( COST_REPAIR );
+		std::cout	<< MSG_CLAPTRAP << this->getName()
+					<< " +" << amount << " repaired: It now has " 
+					<< this->getHitPoint() << " hit points " << "and "
+					<< this->getEnergyPoint() << " energy points." << std::endl;
+	}
+	return ;
+}
