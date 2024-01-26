97a0d7d58aedb13a585e98937fecb8b78320303e
Pierre Olivier Paquet <ppaquet@c1r4p8.42quebec.com>
2024-01-26 14:49:26 -0500

    module_04 Revised

--- module_04/ex00/src/main.cpp
+++ module_04/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:21:08 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/01/25 17:05:50 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/01/26 12:49:11 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,24 @@ void	print_o( std::string to_print, bool nl) {
 	return ;
 }
 
-int	main( int argc, char **argv ) {
-	( void )argc;
-	( void )argv;
-
+void	animal( void ) {
+	print_o("\033[32m\033[1m[ ANIMAL TESTS ]\033[0m", true );
 	print_o( std::string(SEPARATOR) + "[ ANIMAL <meta> ]", true) ;
 	const Animal* meta = new Animal;
 	print_o( std::string(SEPARATOR) + "[ DOG <j> / through Animal ]", true) ;
 	const Animal* j = new Dog;
 	print_o( std::string(SEPARATOR) + "[ CAT <i> / through Animal ]", true) ;
 	const Animal* i = new Cat;
+	
 	print_o( SEPARATOR, true );
 
-	print_o( "j getType: " + j->getType(), true );
-	print_o( "i getType: " + i->getType() + "\n" + SEPARATOR, true );
-	print_o( "j makeSound: ", false);
+	print_o( "<j> getType: " + j->getType(), true );
+	print_o( "<i> getType: " + i->getType() + "\n" + SEPARATOR, true );
+	print_o( "<j> makeSound: ", false);
 	j->makeSound(); // Will output the Dog sound ("Wouf Wouf.")
-	print_o( "i makeSound: ", false);
+	print_o( "<i> makeSound: ", false);
 	i->makeSound(); // Will output the cat sound ("Meow meow.")
-	print_o( "meta makeSound: ", false);
+	print_o( "<meta> makeSound: ", false);
 	meta->makeSound();
 
 	print_o( std::string(SEPARATOR) + "[ ANIMAL <meta> deletion ]", true );
@@ -62,8 +61,10 @@ int	main( int argc, char **argv ) {
 	delete j;
 	print_o( std::string(SEPARATOR) + "[ CAT <i> deletion ]", true );
 	delete i;
-	/**************************************************************************/
-	print_o( std::string("\n") + SEPARATOR + "[ WRONG SECTION ]" "\n", true );
+}
+
+void	wrong_animal( void ) {
+	print_o( std::string("\n") + "\033[31m\033[1m[ WRONG TESTS ]\033[0m" "\n", true );
 	
 	const WrongAnimal	*Metal = NULL;
 	const WrongAnimal	*Hi = NULL;
@@ -73,15 +74,29 @@ int	main( int argc, char **argv ) {
 	print_o( std::string(SEPARATOR) + "[ WRONGANIMAL <Metal> ]", true );
 	Metal = new WrongAnimal;
 
-	std::cout << "WrongAnimal Metal Type > " << Metal->getType() << std::endl;
-	std::cout << "Do you hear Metal? > ";
+	print_o( std::string(SEPARATOR), true );
+	print_o( "<Metal> getType: " + Metal->getType(), true );
+	print_o( "<Metal> makeSound: ", false);
 	Metal->makeSound();
-	std::cout << "WrongAnimal Hi Type > " << Hi->getType() << std::endl;
-	std::cout << "Do you hear Hi? > ";
+	print_o( "<Hi> getType: " + Hi->getType(), true );
+	print_o( "<Hi> makeSound: ", false);
 	Hi->makeSound();
 
+	print_o( std::string(SEPARATOR) + "[ WRONGANIMAL <Metal> deletion ]", true );
 	delete Metal;
+	print_o( std::string(SEPARATOR) + "[ WRONGCAT <Hi> deletion ]", true );
 	delete Hi;
+	print_o( "", true );
+
+	return ;
+}
+
+int	main( int argc, char **argv ) {
+	( void )argc;
+	( void )argv;
+
+	animal();
+	wrong_animal();
 
 	return ( EXIT_SUCCESS );
 }
