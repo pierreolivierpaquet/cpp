eec54ceca0d785e959f96fab426e24b38ff17935
Pierre Olivier <pierreolivierpaquet@hotmail.com>
2024-01-30 17:10:15 -0500

    module_05/ex00 WIP

--- module_05/ex00/include/Bureaucrat.hpp
+++ module_05/ex00/include/Bureaucrat.hpp
@@ -6,34 +6,71 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:12:58 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/01/29 10:32:47 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/01/30 17:08:16 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		BUREAUCRAT_HPP_
 # define	BUREAUCRAT_HPP_
 
-# include <iostream>
+# include	<iostream>
+
+/******************************************************************************/
+
+typedef short	grade_t;
+
+# ifndef	DEFAULT_NAME
+#  define	DEFAULT_NAME	"/* Nameless Bureaucrat */"
+# endif	/*	DEFAULT_NAME	*/
+
+/*	Minimum grade.	*/
+#ifndef		DEFAULT_GRADE
+# define	DEFAULT_GRADE	150
+#endif	/*	DEFAULT_GRADE	*/
+
+/******************************************************************************/
 
 class	Bureaucrat {
 	private:
-		std::string _name;
-		int8_t		_grade;
+		const std::string	_name;
+		grade_t				_grade;
 
 	public:
 	/* Constructors */
 		Bureaucrat( void ); // Default.
 		Bureaucrat( const Bureaucrat &src ); // Reference copy.
-		Bureaucrat( const std::string name ); // Parameterized.
+		
+		Bureaucrat( const std::string new_name ); // Parameterized.
+		Bureaucrat( const grade_t new_grade );
+		Bureaucrat( const std::string new_name, const grade_t new_grade ); // Full parameterized.
 
 	/* Destructor */
 		~Bureaucrat( void ); // Destructor.
 
 		Bureaucrat &operator=( const Bureaucrat &rhs ); // Assignation overload.
-	
+
 	/* Function(s) */
-		void	getName( void ) const;
+		const std::string	getName	( void ) const;
+		grade_t				getGrade( void ) const;
+
+		void	setGrade		( grade_t new_grade );
+		void	incrementGrade	( void );
+		void	incrementGrade	( grade_t amount ); // Substracts <amount>.
+		void	decrementGrade	( void );
+		void	decrementGrade	( grade_t amount ); // Adds <amount>.
+
+	/* Exceptions */
+		class GradeTooHighException : public std::exception {
+			public:
+				const char *what( void ) const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				const char *what( void ) const throw();
+		};
 		
 };	/*	Bureaucrat	*/
+
+std::ostream &operator<<( std::ostream &output, const Bureaucrat &data);
 
 #endif	/*	BUREAUCRAT_HPP_	*/
--- module_05/ex00/src/Bureaucrat.cpp
+++ module_05/ex00/src/Bureaucrat.cpp
@@ -6,8 +6,129 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:13:01 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/01/29 10:15:17 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/01/30 17:09:44 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#include "../include/Bureaucrat.hpp"
+
+/******************************************************************************/
+/********************************* FUNCTIONS **********************************/
+
+const std::string	Bureaucrat::getName( void ) const {
+	return ( this->_name );
+}
+
+grade_t	Bureaucrat::getGrade( void ) const {
+	return ( this->_grade );
+}
+
+void	Bureaucrat::setGrade( grade_t new_grade ) {
+	if (new_grade < 1) {
+		throw( Bureaucrat::GradeTooHighException() );
+	} else if (new_grade > 150) {
+		throw( Bureaucrat::GradeTooLowException() );
+	} else {
+		this->_grade = new_grade;
+	}
+	return ;
+}
+
+void	Bureaucrat::incrementGrade( void ) {
+	this->setGrade( this->getGrade() - 1 );
+	return ;
+}
+
+void	Bureaucrat::incrementGrade( grade_t	amount ) {
+	this->setGrade( this->getGrade() - amount );
+	return ;
+}
+
+void	Bureaucrat::decrementGrade( void ) {
+	this->setGrade( this->getGrade() + 1 );
+	return ;
+}
+
+void	Bureaucrat::decrementGrade( grade_t amount ) {
+	this->setGrade( this->getGrade() + amount );
+	return ;
+}
+
+/******************************************************************************/
+/************************* PARAMETERIZED CONSTRUCTOR **************************/
+
+Bureaucrat::Bureaucrat( const std::string new_name ) :
+	_name( new_name ) {
+	this->setGrade( DEFAULT_GRADE );
+	std::cout	<< this->_name
+				<< " Constructor called [<name> parameterized]" << std::endl;
+	return ;
+}
+
+Bureaucrat::Bureaucrat( const grade_t new_grade ) :
+	_name( DEFAULT_NAME ) {
+	this->setGrade( new_grade );
+	std::cout	<< this->_name
+				<< " Constructor called. [<grade> parameterized]" << std::endl;
+	return ;
+}
+
+Bureaucrat::Bureaucrat( const std::string new_name, const grade_t new_grade ) :
+	_name( new_name ) {
+	this->setGrade( new_grade );
+	std::cout	<< this->_name
+				<< " Constructor called. [full parameterized]" << std::endl;
+	return ;
+}
+
+/******************************************************************************/
+/************************** CANONICAL FORM REQUISITE **************************/
+
+Bureaucrat::Bureaucrat( const Bureaucrat &src ) : 
+	_name( src._name ) {
+	*this = src;
+	std::cout	<< this->_name
+				<< " Constructor called. [reference copy]" << std::endl;
+	return ;
+}
+
+Bureaucrat &Bureaucrat::operator=( const Bureaucrat &rhs ) {
+	this->_grade = rhs._grade;
+	return ( *this );
+}
+
+/******************************************************************************/
+/*********************** DEFAULT CONSTRUCTOR/DESTRUCTOR ***********************/
+
+Bureaucrat::Bureaucrat( void ) :
+	_name( DEFAULT_NAME),
+	_grade( DEFAULT_GRADE ) {
+	std::cout	<< this->_name << " Constructor called. [default]" << std::endl;
+	return ;
+}
+
+Bureaucrat::~Bureaucrat( void ) {
+	std::cout	<< this->_name << " Destructor called. [default]" << std::endl;
+	return ;
+}
+
+/******************************************************************************/
+/******************************* NESTED CLASSES *******************************/
+
+const char *Bureaucrat::GradeTooHighException::what( void ) const throw() {
+	return ( "Grade is too high." );
+}
+
+const char *Bureaucrat::GradeTooLowException::what( void ) const throw() {
+	return ( "Grade is too low." );	
+}
+
+/******************************************************************************/
+/********************************** OPERANDS **********************************/
+
+std::ostream	&operator<<( std::ostream &output, const Bureaucrat &data ) {
+	output	<< data.getName()
+			<< ", bureaucrat grade "
+			<< data.getGrade();
+	return ( output );
+}
