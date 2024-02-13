f99f9978313e3483d729762c7d97b2f5d2ccd77a
Pierre Olivier Paquet <pierreolivierpaquet@hotmail.com>
2024-02-13 14:56:57 -0500

    module_06/ex00: WIP [type id/function pointers dispatch]

--- module_06/ex00/include/CastConversion.hpp
+++ module_06/ex00/include/CastConversion.hpp
@@ -6,7 +6,7 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 07:34:41 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/02/12 15:02:38 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/02/13 14:50:34 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,56 @@
 
 # include	<cstring>
 
-# ifndef	NUM_INCL
-#  define	NUM_INCL	"+-.0123456789f"
-# endif	/*	NUM_EXCL	*/
+# ifndef	FLOAT_CHAR
+#  define	FLOAT_CHAR	"f"
+# endif	/*	FLOAT_CHAR	*/
+
+# ifndef	POINT_CHAR
+#  define	POINT_CHAR	"."
+# endif	/*	DOUBLE_CHAR	*/
+
+# ifndef	SIGN_CHARS
+#  define	SIGN_CHARS	"+-"
+# endif	/*	SIGN_CHARS	*/
+
+# ifndef	DIGIT_CHARS
+#  define	DIGIT_CHARS	"0123456789"
+# endif	/*	DIGIT_CHARS	*/
+
+# ifndef	NOT_FOUND
+#  define	NOT_FOUND	std::string::npos
+# endif	/*	NOT_FOUND	*/
 
 class Conversion {
 	private:
 		typedef enum conv {
-			C_UNDEFINED,
-			C_CHAR,
-			C_INT,
-			C_FLOAT,
-			C_DOUBLE,
-			C_NAN,
-			C_NANF,
-			C_INF,
-			C_ERROR = 100
+			C_ERROR,
+			C_NAN,		//	nan
+			C_NANF,		//	nanf
+			C_INF,		//	+inf
+			C_MINF,		//	-inf
+			C_INFF,		//	+inff
+			C_MINFF,	//	-inff
+			C_CHAR,		//	character
+			C_INT,		//	integer
+			C_DOUBLE,	//	double
+			C_FLOAT,	//	float
+			C_UNDEFINED
 		} conv_type_t;
 
+		bool	isError(	std::string input ) const;
+		bool	isNan(		std::string input ) const;
+		bool	isNanf(		std::string input ) const;
+		bool	isInf(		std::string input ) const;
+		bool	isInff(		std::string input ) const;
+		bool	isMinf(		std::string input ) const;	//	Minus Inf
+		bool	isMinff(	std::string input ) const;	//	Minus Inff
+		bool	isChar(		std::string input ) const;
+		bool	isInt(		std::string input ) const;
+		bool	isDouble(	std::string input ) const;
+		bool	isFloat(	std::string input ) const;
+
+//	----------------------------------------------------------------------------
 		const std::string	_program_input;
 		conv_type_t			_conversion_type;
 		
@@ -44,7 +76,9 @@ class Conversion {
 		double				_double_cast;
 		char				_char_cast;
 
-		void	_setType( void );
+		typedef	bool(Conversion::*typeIdentifier)(std::string input) const;
+		typeIdentifier	_type_id[ C_UNDEFINED ];
+		void			_setType( void );
 
 	public:
 //	CONSTRUCTOR.S --------------------------------------------------------------
@@ -58,12 +92,12 @@ class Conversion {
 		Conversion &operator=( const Conversion &rhs );
 
 //	GETTER.S -------------------------------------------------------------------
-		conv_type_t 	getConversionType( void ) const;
-		std::string		getProgramInput( void ) const;
-		char			getChar( void ) const;
-		int				getInt( void ) const;
-		float			getFloat( void ) const;
-		double			getDouble( void ) const;
+		conv_type_t 	getConversionType(	void ) const;
+		std::string		getProgramInput(	void ) const;
+		char			getChar(			void ) const;
+		int				getInt(				void ) const;
+		float			getFloat(			void ) const;
+		double			getDouble(			void ) const;
 
 //	SETTER.S -------------------------------------------------------------------
 
@@ -73,4 +107,3 @@ class Conversion {
 };	//	Conversion
 
 #endif	/*	CASTCONVERSION_HPP_	*/
-
--- module_06/ex00/src/CastConversion.cpp
+++ module_06/ex00/src/CastConversion.cpp
@@ -6,35 +6,127 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 12:35:33 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/02/12 15:04:24 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/02/13 14:53:38 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/CastConversion.hpp"
 
-typedef std::string::const_iterator const_iterator_t;
+// static std::size_t	skipSigns( std::string input ) {
+// 	std::size_t	index = 0;
+// 	for (std::string::iterator it = input.begin(); it != input.end(); it++) {
+// 		if (*it != '+' && *it != '-'){
+// 			return ( index );
+// 		}
+// 		index++;
+// 	}
+// 	return ( index );
+// }
+
+// typedef std::string::const_iterator const_iterator_t;
+
+bool	Conversion::isError( std::string input ) const {
+	if (input.empty() == true || \
+		(input.find_first_of(".") != input.find_last_of(".")))
+		return ( true );
+	return ( false );
+}
+
+bool	Conversion::isNan( std::string input ) const {
+	if (input.compare("nan") == 0){
+		return ( true );
+	}
+	return ( false );
+}
+
+bool	Conversion::isNanf( std::string input ) const {
+	if (input.compare("nanf") != 0){
+		return ( false );
+	}
+	return ( true );
+}
+
+bool	Conversion::isInf( std::string input ) const {
+	if (input.compare("+inf") != 0){
+		return ( false );
+	}
+	return ( true );
+}
+
+bool	Conversion::isInff( std::string input ) const {
+	if (input.compare("+inff") != 0){
+		return ( false );
+	}
+	return ( true );
+}
+
+bool	Conversion::isMinf( std::string input ) const {
+	if (input.compare("-inf") != 0){
+		return ( false );
+	}
+	return ( true );
+}
+
+bool	Conversion::isMinff( std::string input ) const {
+	if (input.compare("-inff") != 0){
+		return ( false );
+	}
+	return ( true );
+}
+
+bool	Conversion::isChar( std::string input ) const {
+	if (input.length() > 1) {
+		return ( false );
+	} else if (input.find_first_of( DIGIT_CHARS ) != NOT_FOUND) {
+		return ( false );
+	}
+	return ( true );
+}
+
+bool	Conversion::isInt( std::string input ) const {
+	if (input.find_first_not_of( static_cast<std::string>(SIGN_CHARS) + DIGIT_CHARS ) == NOT_FOUND) {
+		return ( true );
+	}
+	return ( false );
+}
+
+bool	Conversion::isDouble( std::string input ) const {
+	if (input.length() < 2)
+		return ( false );
+	if (input.find_first_not_of( (static_cast<std::string>(DIGIT_CHARS)
+				+ POINT_CHAR + SIGN_CHARS) ) != NOT_FOUND)
+		return ( false );
+	return ( true );
+}
+
+bool	Conversion::isFloat( std::string input ) const {
+	if (input.length() < 2 || \
+		input.find("f") == NOT_FOUND || \
+		input.find_first_not_of( SIGN_CHARS ) == input.find(".f")) {
+		return ( false );
+	} else if (	input.find_first_not_of( (static_cast<std::string>(FLOAT_CHAR)
+				+ DIGIT_CHARS + POINT_CHAR + SIGN_CHARS) ) != NOT_FOUND) {
+				return ( false );
+	}
+	if ( input.find( 'f' ) - 1 == input.find_last_of( SIGN_CHARS ) ) {
+		return ( false );
+	}
+	return ( true );
+}
 
 /********************************* FUNCTION.S *********************************/
 
 void	Conversion::_setType( void ) {
-	if (this->_conversion_type != C_UNDEFINED){
-		return ;
+	for (int i = 0; i < C_UNDEFINED; i++){
+		if ((this->*_type_id[ i ])( this->_program_input ) == true) {
+			this->_conversion_type = static_cast<conv_type_t>( i );
+			break;
+		}
 	}
-	// CHAR
-
-	// DOUBLE
-
-	// FLOAT (lenght > 1) && ()
-	if (this->_program_input.length() > 1 && \
-		this->getProgramInput().find_first_not_of(NUM_INCL)) {
-		this->_conversion_type = C_FLOAT;
-	}
-	// INT
 	return ;
 }
 
 /********************************** SETTER.S **********************************/
-
 
 
 /********************************** GETTER.S **********************************/
@@ -83,9 +175,22 @@ Conversion &Conversion::operator=( const Conversion &rhs ) {
 /********************* DEFAULT CONSTRUCTOR && DESTRUCTOR **********************/
 
 Conversion::Conversion( std::string program_input ) :
-	_program_input( program_input ) {
+	_program_input( program_input ),
+	_conversion_type( C_UNDEFINED ) {
 	std::cout	<< this->_program_input
 				<< " Constructor called [default/parameterized]" << std::endl;
+	_type_id[ C_ERROR	] = &Conversion::isError;
+	_type_id[ C_NAN		] = &Conversion::isNan;
+	_type_id[ C_NANF	] = &Conversion::isNanf;
+	_type_id[ C_INF		] = &Conversion::isInf;
+	_type_id[ C_MINF	] = &Conversion::isMinf;
+	_type_id[ C_INFF	] = &Conversion::isInff;
+	_type_id[ C_MINFF	] = &Conversion::isMinff;
+	_type_id[ C_CHAR	] = &Conversion::isChar;
+	_type_id[ C_INT		] = &Conversion::isInt;
+	_type_id[ C_DOUBLE	] = &Conversion::isDouble;
+	_type_id[ C_FLOAT	] = &Conversion::isFloat;
+	
 	this->_setType();
 	return ;
 }
@@ -95,4 +200,3 @@ Conversion::~Conversion( void ) {
 				<< "Destructor called. [default]" << std::endl;
 	return ;
 }
-
