9f39d81b456ce007ce202a7fe409fce5a5bdaf32
Pierre Olivier Paquet <pierreolivierpaquet@hotmail.com>
2024-02-23 15:20:48 -0500

    module_07/ex00:: WIP

--- module_07/ex00/src/main.cpp
+++ module_07/ex00/src/main.cpp
@@ -6,7 +6,38 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 15:07:14 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/02/22 15:07:15 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/02/23 15:19:17 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/Whatever.hpp"
+
+int main ( int argc, char **argv) {
+	static_cast<void>( argc );
+	static_cast<void>( argv );
+
+	int a = 2;
+	int b = 3;
+
+	for (size_t i = 0; i < 1000; i++)
+	{
+		std::cout << i << std::endl;
+	}
+	
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+
+	return (0);
+}
