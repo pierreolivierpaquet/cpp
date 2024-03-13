2ec6f82dcb45d4a7845406ab2eedd8bf9ff65ff9
Pierre Olivier Paquet <pierreolivierpaquet@hotmail.com>
2024-03-13 13:37:59 -0400

    module_09/ex02: CREATED

---
+++ module_09/ex02/include/PmergeMe.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 13:28:04 by ppaquet           #+#    #+#             */
+/*   Updated: 2024/03/13 13:33:16 by ppaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		PMERGEME_HPP_
+# define	PMERGEME_HPP_
+
+# include	<iostream>
+# include	<algorithm>	// tri
+
+///	----------------------------------------------- @section MACRO.S - TYPEDEF.S
+
+# ifndef	EXIT_SUCCESS
+#  define	EXIT_SUCCESS	0
+# endif	/*	EXIT_SUCCESS	*/
+
+///	------------------------------------------------------------------- @section
+
+#endif	/*	PMERGEME_HPP_	*/
