b85f89e873276c37d0692c09122c3bd35c32e4a1
Pierre Olivier Paquet <pierreolivierpaquet@hotmail.com>
2024-03-15 15:03:05 -0400

    module_09/ex02: WIP [ paring & first sorta added; mergeSort() + merge() functions added ]

--- module_09/ex02/include/PmergeMe.hpp
+++ module_09/ex02/include/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:28:04 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/03/14 14:44:56 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/03/15 13:19:24 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,20 @@
 # include	<list> // test
 # include	<stack>
 # include	<sstream>	// istringstream
-
+# include	<limits>	// numeric_limits
+# include	<cstdlib>	// u_int32_t
+# include	<utility>	// swap
 
 ///	----------------------------------------------- @section MACRO.S - TYPEDEF.S
 
 # ifndef	EXIT_SUCCESS
 #  define	EXIT_SUCCESS	0
 # endif	/*	EXIT_SUCCESS	*/
+
+# define	ERR_MSG			"\033[1;31merror\033[0m"
+# define	ERR_OVERFLOW	": unsupported value detected: "
+# define	ERR_BAD_INPUT	": non-numeric input detected."
+# define	DIGIT_CHAR		"+-0123456789"
 
 typedef enum {
 	DEQUE,
@@ -36,23 +43,53 @@ typedef enum {
 typedef std::deque< std::pair< int, int > >		deque_t;
 typedef std::vector< std::pair< int, int > >	vector_t;
 
-///	------------------------------------------------------------------- @section
+///	---------------------------------------------------------- @section CLASSE.S
 
-template < typename Container = std::deque< std::pair< int, int > > >
+template < typename Container = deque_t >
 class	PmergeMe : public Container {
 	private:
-		e_type_info				_ti;
-		const std::type_info	&_type_info;
-		std::string				_input;
+		PmergeMe( void );
+		const std::vector< u_int32_t >	_unsorted_vector;
+		const std::deque< u_int32_t >	_unsorted_deque;
+		e_type_info						_ti;
+		const std::type_info			&_type_info;
+		u_int32_t						_size;
+
+		void	_pairSort( void ); // first sorting step.
+		void	_mergeSort( Container &array );
+		void	_merge( Container &lhs, Container &rhs, Container &array );
+		void	_pairing(	std::vector< u_int32_t >::const_iterator it );
+		void	_pairing(	std::deque< u_int32_t >::const_iterator it );
+
+		std::pair< int, int >	_straggler;
 
 	public:
-		PmergeMe(	void );
-		virtual ~PmergeMe(	void );
+///	------------------------------------------------- @class SETTER.S - GETTER.S
+
+		void		setType( void );
 		std::string	getTypeInfo( void ) const;
 
-		void	setInput( std::string input );
+///	------------------------------------------------------- @class CONSTRUCTOR.S
+
+		PmergeMe( int ac, char **av );
+
+///	-------------------------------------------------------- @class DESTRUCTOR.S
+
+		virtual ~PmergeMe(	void );
+
+///	------------------------------------------------- @class OPERATOR.S OVERLOAD
+
+
+
+///	---------------------------------------------------------- @class FUNCTION.S
+
+	void	algorithm( void );
 
 };	/*	PmergeMe	*/
+
+// std::vector< u_int32_t > input_check( int ac, char **av );
+template < typename C >
+C input_check( int ac, char **av );
 
 # include "../include/PmergeMe.tpp"
 
--- module_09/ex02/src/main.cpp
+++ module_09/ex02/src/main.cpp
@@ -6,40 +6,18 @@
 /*   By: ppaquet <pierreolivierpaquet@hotmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:28:02 by ppaquet           #+#    #+#             */
-/*   Updated: 2024/03/14 14:10:14 by ppaquet          ###   ########.fr       */
+/*   Updated: 2024/03/15 12:25:50 by ppaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PmergeMe.hpp"
 
 int	main( int argc, char **argv ) {
-
-	PmergeMe< vector_t >	test;
-
-	std::pair< int, int > pair;
-	std::istringstream	tmp;
-	int	number = 0;
-	for (int i = 1; i < argc; i++) {
-		tmp.str( argv[ i ] );
-		tmp >> number;
-		if (i % 2 && i < argc - 1) {
-			pair.first = number;
-		} else {
-			pair.second = number;
-		}
-		if (i % 2 == 0) {
-			test.push_back( pair );
-			pair = std::make_pair( 0, 0 );
-		} else if ( i == argc - 1) {
-			test.push_back( pair );
-		}
-		number = 0;
-		tmp.clear();
+	try {
+		PmergeMe< deque_t >	test( argc, argv );
+		test.algorithm();
+	} catch ( std::exception &e ) {
+		std::cout << e.what() << std::endl;
 	}
 	return ( EXIT_SUCCESS );
 }
-
-/**
- * # -- 1.	Take string input, turn it into a stack of int (deque).
- * # -- 2.	
-*/
